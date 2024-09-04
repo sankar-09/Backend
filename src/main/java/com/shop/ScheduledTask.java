344ac79392a44b4790cb7886860d3dc88cbfad7d
Sankar <settisankar95@gmail.com>
2024-09-05 00:46:06 +0530

    Update ScheduledTask.java

--- src/main/java/com/shop/ScheduledTask.java
+++ src/main/java/com/shop/ScheduledTask.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 public class ScheduledTask {
 
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String url = "https://shopflowbackend-hsz6.onrender.com"; // Replace with your actual endpoint if needed
+    private final String url = "https://backend-y0hv.onrender.com"; // Replace with your actual endpoint if needed
 
     @Scheduled(fixedRate = 300000) // Run every 5 minutes
     public void pingServer() {
